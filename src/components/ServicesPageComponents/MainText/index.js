9a5b05101de68ee4a6ade07ab1952551fab54111
Alex <alexandrelopezcivallero@gmail.com>
2021-03-06 20:34:59 +0100

    page service

---
+++ src/components/ServicesPageComponents/MainText/index.js
@@ -0,0 +1,43 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { ElementBox, ElementContainer, ElementParagraph} from "./MainTextElements"
+import Image from "./../Image"
+
+
+const MainText = () => {
+    return (
+        <>
+
+
+        <ElementContainer>
+        <ElementBox/>
+        <ElementParagraph>
+
+        <h3>Hugo</h3>
+        At Wellthy Clinic we offer a full body service with Osteopathy, Nutritionist and Pilates services. To support better health and wellness through meaningful treatment, nourishment and movement
+        </ElementParagraph>
+        </ElementContainer>
+        
+        <ElementContainer>
+        <ElementParagraph>
+        <h3>Integrated Approach</h3>
+        We believe that by making a number of small changes in different domains is better than one big change in a single area. This is why we have an integrated approach combining knowledge learnt from different fields into treatments to gain the best results for patients.
+        </ElementParagraph>
+        <ElementBox/>
+        </ElementContainer>
+
+
+        <ElementContainer>
+        <ElementBox/>
+        <ElementParagraph>
+        <h3>Advice service</h3>
+        We will advise on best stretching and strengthening home exercises and stress management strategies. Also top hydration and nutrition tips, heat therapy and sleep hygiene techniques to optimise healing.
+        </ElementParagraph>  
+        </ElementContainer>
+
+        </>
+    )
+}
+
+export default MainText
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,11 +2,13 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import  MainText from "../components/ServicesPageComponents/MainText"
 
 const Services = () => (
     <Layout>
         <SEO title="page 2" />
         <h1>Services</h1>
+        <MainText></MainText>
     </Layout>
 )
 
