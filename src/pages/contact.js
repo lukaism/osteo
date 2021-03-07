875f7514b87ab0b2eaaf26ce4d131dee21b07332
Alex <alexandrelopezcivallero@gmail.com>
2021-03-07 17:37:55 +0100

    addcontact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,13 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import ContactInfo from "../components/ContactInfo"
 const Contact = () => (
     <Layout>
         <SEO title="page 2" />
         <h1>Contact</h1>
+        <ContactInfo/>
+    
     </Layout>
 )
 
